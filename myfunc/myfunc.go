f0323f44e4a69a964c0769bb8e56c9b05a0a9f7c
anndad <624357199@qq.com>
2022-06-17 00:55:25 +0800

    修改注释

--- myfunc/myfunc.go
+++ myfunc/myfunc.go
@@ -496,7 +496,7 @@ func ShellCmd(cmd string) (string, error) {
 	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
 	var out bytes.Buffer
 	command.Stdout = &out
-	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	//Run执行c包含的命令，并阻塞直到完成。这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
 	err := command.Run()
 	return out.String(), err
 }
