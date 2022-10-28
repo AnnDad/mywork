25cb0010e45f0fa98baa7f739f4a1738328a7287
anndad <624357199@qq.com>
2022-10-28 09:28:41 +0800

    mytask.go: 增加executeWithDone函数

--- mytask/mytask.go
+++ mytask/mytask.go
@@ -14,34 +14,49 @@ type TOnExecute func(taskData string) (string, error)
 //某个任务执行完成后
 //输入: data 执行结果, err 执行过程中是否有错误
 //输出: bool 是否继续等待完成, true->继续等待,false->不等待
-type TOnDone func(data string, err error) bool
 
-func Execute(Func_Exec TOnExecute, Func_Done TOnDone, taskData ...string) (*myjson.TJson, error) {
-	chDone := make(chan string)
-	results := myjson.New()
+type TOnDone func(tasks *myjson.TJson, data string, err error) bool
+
+func Execute(Func_Exec TOnExecute, taskData ...string) *myjson.TJson {
 	TaskCount := len(taskData)
+	chDone := make(chan string, TaskCount)
+	tasks := myjson.New()
 	for i, item := range taskData {
 		go execInThread("thread"+myfunc.String(i), item, Func_Exec, chDone)
 	}
 	//wait
-	for i := 0; i < this.TaskCount; i++ {
-		result := <-this.chDone
-		json := myjson.New(result)
-		if Func_Done != nil {
-			var err error
-			err_msg := json.GetString("task.err")
-			data := json.GetString("task.data")
-			if err_msg != "ok" {
-				err = myfunc.NewError(err_msg)
-			}
-			is_abort := Func_Done(data, err)
-			if !is_abort {
-				break
-			}
-		}
-		results.Append("results", json.Get("task"))
+	for i := 0; i < TaskCount; i++ {
+		result := <-chDone
+		jsResult := myjson.New(result)
+		tasks.Append("tasks", jsResult.Get("task"))
 	}
-	return results
+	return tasks
+}
+
+func ExecuteWithDone(Func_Exec TOnExecute, Func_Done TOnDone, taskData ...string) *myjson.TJson {
+	TaskCount := len(taskData)
+	chDone := make(chan string, TaskCount)
+	tasks := myjson.New()
+	for i, item := range taskData {
+		go execInThread("thread"+myfunc.String(i), item, Func_Exec, chDone)
+	}
+	//wait
+	for i := 0; i < TaskCount; i++ {
+		result := <-chDone
+		jsResult := myjson.New(result)
+		var err error
+		err_msg := jsResult.GetString("task.msg")
+		data := jsResult.GetString("task.data")
+		if err_msg != "ok" {
+			err = myfunc.NewError(err_msg)
+		}
+		is_Continue := Func_Done(tasks, data, err)
+		if !is_Continue {
+			break
+		}
+		tasks.Append("tasks", jsResult.Get("task"))
+	}
+	return tasks
 }
 
 func execInThread(threadid, taskData string, Func_Exec TOnExecute, chDone chan string) {
@@ -56,5 +71,5 @@ func execInThread(threadid, taskData string, Func_Exec TOnExecute, chDone chan s
 	if err != nil {
 		msg = "err: " + err.Error()
 	}
-	result.Set("task.err", msg)
+	result.Set("task.msg", msg)
 }
